c77505d80a941550efe6396abb429192621f215a
Jan Kandrac <kandrac.jan@gmail.com>
2019-09-18 05:31:55 +0200

    Lecture-2 - full basic

---
+++ lecture-1/src/sk/ssnd/Creature.java
@@ -0,0 +1,71 @@
+package sk.ssnd;
+
+import java.util.Random;
+
+public class Creature {
+
+    private int maxHitPoints;
+    private int hitPoints;
+    private int armor;
+    private int strengthLowest;
+    private int strengthHighest;
+
+    public Creature(int hitPoints, int armor, int strengthLowest, int strengthHighest) {
+        this.maxHitPoints = hitPoints;
+        this.hitPoints = hitPoints;
+        this.armor = armor;
+        this.strengthLowest = strengthLowest;
+        this.strengthHighest = strengthHighest;
+    }
+
+    /**
+     * [EN]
+     * Print actual status of this creature
+     *
+     * [SK]
+     * Vypis aktualny stav tejto bytosti
+     */
+    public void printStatus() {
+        System.out.println(hitPoints + " / " + maxHitPoints);
+    }
+
+    /**
+     * [EN]
+     * Attack other {@link Creature}.
+     *
+     * Damage dealt is computed this way:
+     * 1. compute defense strength - 100 armor means no damage dealt 0 armor means 100% damage dealt
+     * 2. compute attack strength - random number from interval strengthLowest - strengthHighest
+     * 3. apply multiplier on attacking creature strength
+     *
+     * [SK]
+     * Zautoc na inu bytost
+     *
+     * Damage ktory udelim sa vypocita takto:
+     * 1. vypocitaj obrannu silu - 100 armor znamena 0% udeleneho damage; 0 armor znamena 100% damage
+     * 2. vypocitaj obrannu silu - nahodne cislo z intervalu strengthLowest - strengthHighest
+     * 3. aplikuj obrannu silu na silu utociacej jednotky
+     *
+     * @param defender creature to be attacked by this creature
+     */
+    public void attackCreature(Creature defender) {
+        Random random = new Random();
+        int strength = random.nextInt(strengthHighest - strengthLowest) + strengthLowest;
+        double multiplier = ((double) (100 - defender.armor)) / 100.0;
+        double damageDealt = strength * multiplier;
+        defender.hitPoints -= damageDealt;
+    }
+
+    /**
+     * [EN]
+     * Compute whether this creature is alive or not
+     *
+     * [SK]
+     * Vypocitaj, ci je tato bytost nazive alebo nie
+     *
+     * @return true if alive
+     */
+    public boolean isAlive() {
+        return hitPoints > 0;
+    }
+}
---
+++ lecture-1/src/sk/ssnd/Main.java
@@ -0,0 +1,24 @@
+package sk.ssnd;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Creature creature1 = new Creature(1000, 20, 200, 250);
+        Creature creature2 = new Creature(2000, 0, 120, 180);
+
+        while (creature1.isAlive() && creature2.isAlive()) {
+            creature1.attackCreature(creature2);
+            creature2.attackCreature(creature1);
+        }
+
+        if (creature1.isAlive()) {
+            System.out.print("Creature 1 WON - ");
+            creature1.printStatus();
+        } else if (creature2.isAlive()){
+            System.out.print("Creature 2 WON - ");
+            creature2.printStatus();
+        } else {
+            System.out.print("DRAW");
+        }
+    }
+}
